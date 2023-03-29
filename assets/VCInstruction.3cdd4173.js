c6bcf545d2129909ba656e9fc40a8b4855ef564b
raghadsalameh1 <90514155+raghadsalameh1@users.noreply.github.com>
2023-03-29 06:11:01 +0300

    fix - score sometimes not apear

---
+++ assets/VCInstruction.3cdd4173.js
@@ -0,0 +1,1 @@
+import{V as t}from"./VcInstructions.1e437464.js";import{_ as n}from"./VAvatar.e5d3d1b3.js";import{X as r,Y as s,a1 as c}from"./index.3fa4bb8d.js";import"./VCard.c0a24b9d.js";import"./VRow.114c1365.js";const e={components:{VcInstructions:t}};function p(a,i,m,_,u,f){const o=r("VcInstructions");return s(),c(o,{Showstart:!0})}const x=n(e,[["render",p]]);export{x as default};
---
+++ assets/VSpacer.61fc29da.js
@@ -0,0 +1,1 @@
+import{l as e}from"./VAvatar.e5d3d1b3.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
