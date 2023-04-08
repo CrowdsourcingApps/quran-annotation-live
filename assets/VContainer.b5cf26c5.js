b26fc136605e4c452fe043c91e842aec6587123e
raghadsalameh1 <90514155+raghadsalameh1@users.noreply.github.com>
2023-04-09 01:56:29 +0300

    Add contactus page and about us page

---
+++ assets/VContainer.b5cf26c5.js
@@ -0,0 +1,1 @@
+import"./VRow.7a70a52c.js";import{m as t,u as n}from"./VAvatar.38372d38.js";import{x as r,m as s}from"./index.7fb740c4.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
